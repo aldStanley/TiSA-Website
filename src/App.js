aac77830be5a918cee71488d32c1146fcc0b0bdb
Stanley Chung <98046927+aldStanley@users.noreply.github.com>
2023-01-22 21:52:29 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+import "./App.css";
+import {Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+import AboutUs from "./components/AboutUs"
+import Default from "./components/Default";
+import Events from "./components/Events"
+import Home from "./components/Home"
+import Members from "./components/Members"
+import Navbar from "./components/Navbar";
+import PhotoGallery from "./components/PhotoGallery"
+import oneOone from ".//image/101pic.png"
+import bg from "./image/bg.png"
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <div style={{backgroundImage: `url(${oneOone})`, backgroundRepeat:"no-repeat",backgroundSize:"contain", height:1056, width:1848}}>
+      
+      <Navbar />
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/AboutUs" element={<AboutUs/>} />
+        <Route path="/Events" element={<Events/>} />
+        <Route path="/Members" element={<Members/>}/>
+        <Route path="/Navbar" element={<Navbar/>}/>
+        <Route path="/PhotoGallery" element={<PhotoGallery/>}/>
+        <Route path="*" element={<Default/>} />
+      </Routes>
+      </div>
+      
+      
+      
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PhotoGallery extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from PhotoGallery</h3>
+      </div>
+    )
+  }
+}
